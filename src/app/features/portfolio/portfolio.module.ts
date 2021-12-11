b506667cf975e2ae52d1519491f2de9b5da4364c
Jonas Wolfram <gartenspaten@web.de>
2021-12-11 16:39:17 +0100

    Created section header

--- src/app/features/portfolio/portfolio.module.ts
+++ src/app/features/portfolio/portfolio.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PortfolioRoutingModule } from './portfolio-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { AboutSectionComponent } from './components/about-section/about-section.component';
+import { SectionHeaderComponent } from './components/section-header/section-header.component';
 
 @NgModule({
-  declarations: [HomeComponent, HeroSectionComponent],
+  declarations: [HomeComponent, HeroSectionComponent, AboutSectionComponent, SectionHeaderComponent],
   imports: [CommonModule, PortfolioRoutingModule],
 })
 export class PortfolioModule {}
