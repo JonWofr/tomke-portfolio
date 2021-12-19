7518a30bcc98e962829b5477e678cac6fddd22bc
Jonas Wolfram <gartenspaten@web.de>
2021-12-19 13:13:55 +0100

    Included contact button animation

--- src/app/features/portfolio/components/hero-section/hero-section.component.ts
+++ src/app/features/portfolio/components/hero-section/hero-section.component.ts
@@ -15,35 +15,95 @@ import {
 })
 export class HeroSectionComponent implements OnInit, AfterViewInit {
   @Output() changeHeroTitleIntersection = new EventEmitter<boolean>();
+
+  isContactButtonIntersecting?: boolean;
+  contactButtonAnimationQueue: boolean[] = [];
+  isContactButtonAnimationRunning = false;
+  private readonly intersectionObserverOptions: IntersectionObserverInit = {
+    rootMargin: '-64px 0px 0px 0px',
+    threshold: 1,
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
-    this.initialiseIntersectionObserver();
-  }
-
-  initialiseIntersectionObserver() {
-    const intersectionObserver = this.createIntersectionObserver();
-    const heroTitle = document.querySelector('.hero__title');
-    if (heroTitle) intersectionObserver.observe(heroTitle);
-  }
-
-  createIntersectionObserver() {
-    const intersectionObserverOptions: IntersectionObserverInit = {
-      rootMargin: '-64px 0px 0px 0px',
-      threshold: 1,
-    };
-    const intersectionObserver = new IntersectionObserver(
-      this.intersectionObserverCallback.bind(this),
-      intersectionObserverOptions
+    this.initialiseIntersectionObserver(
+      '.hero__title',
+      this.titleIntersectionObserverCallback.bind(this)
     );
-    return intersectionObserver;
+    this.initialiseIntersectionObserver(
+      '.hero__contact-button-trigger',
+      this.contactButtonIntersectionObserverCallback.bind(this)
+    );
   }
 
-  intersectionObserverCallback(entries: IntersectionObserverEntry[]) {
+  initialiseIntersectionObserver(
+    elementQuerySelector: string,
+    intersectionObserverCallback: IntersectionObserverCallback
+  ) {
+    const intersectionObserver = new IntersectionObserver(
+      intersectionObserverCallback,
+      this.intersectionObserverOptions
+    );
+    const element = document.querySelector(elementQuerySelector);
+    if (element) intersectionObserver.observe(element);
+  }
+
+  titleIntersectionObserverCallback(entries: IntersectionObserverEntry[]) {
     entries.forEach((entry) =>
       this.changeHeroTitleIntersection.emit(entry.isIntersecting)
     );
+  }
+
+  contactButtonIntersectionObserverCallback(
+    entries: IntersectionObserverEntry[]
+  ) {
+    entries.forEach((entry) => {
+      console.log('triggered', entry);
+      // No animation should happen when the button is initialised
+      if (this.isContactButtonIntersecting === undefined) {
+        this.isContactButtonIntersecting = entry.isIntersecting;
+        return;
+      }
+
+      this.handleContactButtonAnimation(entry.isIntersecting);
+    });
+  }
+
+  handleContactButtonAnimation(isIntersecting: boolean) {
+    if (!this.isContactButtonAnimationRunning) {
+      this.startContactButtonAnimation(isIntersecting);
+      // Set to the total amount of the CSS transition-duration plus a buffer of 50ms
+      this.setTimeoutPromise(550).then(
+        this.stopContactButtonAnimation.bind(this)
+      );
+    } else {
+      this.contactButtonAnimationQueue.push(isIntersecting);
+    }
+  }
+
+  startContactButtonAnimation(isIntersecting: boolean) {
+    this.isContactButtonIntersecting = isIntersecting;
+    this.isContactButtonAnimationRunning = true;
+  }
+
+  setTimeoutPromise(ms: number): Promise<void> {
+    return new Promise((resolve, _) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  stopContactButtonAnimation() {
+    this.isContactButtonAnimationRunning = false;
+    if (this.contactButtonAnimationQueue.length > 0) {
+      const isIntersecting =
+        this.contactButtonAnimationQueue[
+          this.contactButtonAnimationQueue.length - 1
+        ];
+      this.contactButtonAnimationQueue = [];
+      this.handleContactButtonAnimation(isIntersecting);
+    }
   }
 }
